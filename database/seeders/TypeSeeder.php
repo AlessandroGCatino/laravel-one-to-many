a06b92065afa5db0301b2ce693118e2fce62c658
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-04-03 16:42:21 +0200

    definito seeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,17 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = [
+            "Autonomo",
+            "Di Gruppo",
+            "Sperimentale",
+            "FullStack"
+        ];
+
+        foreach($types as $element){
+            $new_type = new Type();
+            $new_type->name = $element;
+            $new_type->save();
+        }
     }
 }
